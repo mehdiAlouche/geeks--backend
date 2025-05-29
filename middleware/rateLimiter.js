fb455a096656296a33926b016810b3172592e452
mehdiAlouch <idouallouchmehdi@gmail.com>
2025-05-29 11:16:53 +0100

    uncreas the limit

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,11 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
 exports.loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 attempts
+  windowMs: 15 * 60 * 1, // 15 minutes
+  max: 100, // 5 attempts
   message: {
     success: false,
-    message: 'Too many login attempts, please try again after 15 minutes'
+    message: 'Too many login attempts, please try again after 1 minutes'
   }
 });
 
